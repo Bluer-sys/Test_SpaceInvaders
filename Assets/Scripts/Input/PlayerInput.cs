58aa1be61580b142dca047c3b0d6d77283b1c26a
Ilya Novikov <ilianovi@mail.ru>
2024-04-13 17:31:01 +0300

    Input fix

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -8,24 +8,65 @@ namespace Input
 	[UsedImplicitly]
 	public class PlayerInput : IPlayerInput, ITickable
 	{
-		private Vector2 _prevMousePos;
-
+		private Vector2 _prevInputPos;
+		private bool _beginTouchHappened;
+		
 		public ReactiveProperty<Vector2> Delta { get; } = new();
 
 		public void Tick()
+		{
+#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+			MobileInput();
+#else
+			DesktopInput();
+#endif
+		}
+		
+		private void MobileInput()
+		{
+			if ( UnityEngine.Input.touchCount == 0 )
+			{
+				_beginTouchHappened = false;
+				return;
+			}
+
+			Vector2 curTouchPos = UnityEngine.Input.GetTouch( 0 ).position;
+
+			if ( !_beginTouchHappened )
+			{
+				BeginTouch( curTouchPos );
+				_beginTouchHappened = true;
+				return;
+			}
+
+			SetDelta( curTouchPos );
+			_prevInputPos = curTouchPos;
+		}
+
+		private void DesktopInput()
 		{
 			Vector2 curMousePos = UnityEngine.Input.mousePosition;
             
 			if ( UnityEngine.Input.GetMouseButton( 0 ) )
 			{
-				Vector2 delta = curMousePos - _prevMousePos;
-
-				Delta.Value = delta.magnitude < float.Epsilon 
-					? Vector2.zero 
-					: delta;
+				SetDelta( curMousePos );
 			}
 			
-			_prevMousePos = curMousePos;
+			_prevInputPos = curMousePos;
+		}
+
+		private void BeginTouch(Vector2 curTouchPos)
+		{
+			_prevInputPos = curTouchPos;
+		}
+
+		private void SetDelta(Vector2 curInputPos)
+		{
+			Vector2 delta = curInputPos - _prevInputPos;
+			
+			Delta.Value = delta.magnitude < float.Epsilon 
+				? Vector2.zero 
+				: delta;
 		}
 	}
 }
